4187855c1b5fb4125447250a4189e8806dbd53e6
TmRAaEx <tmr.aaex@gmail.com>
2025-05-15 11:36:34 +0200

    created user model

---
+++ newsletter-app/app/Models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+
+class User extends Model
+{
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = [
+        'first_name',
+        'last_name',
+        'email',
+        'password_hash',
+        'salt',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+}
