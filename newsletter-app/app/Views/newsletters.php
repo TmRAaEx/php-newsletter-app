396ca2f7924a37a3800f14f525a46f2a77c5ce35
tmraaex <tmr.aaex@gmail.com>
2025-05-17 01:32:41 +0200

    Created view for newsletters with a component for modularity

---
+++ newsletter-app/app/Views/newsletters.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+    .newsletters ul {
+        display: grid;
+        padding: 0;
+        list-style: none;
+        grid-template-columns: repeat(2, 1fr);
+        gap: clamp(1rem, 5vw, 2rem);
+    }
+
+    @media screen and (min-width: 768px) {
+        .newsletters ul {
+            grid-template-columns: repeat(3, 1fr);
+        }
+    }
+
+    @media screen and (min-width: 1024px) {
+        .newsletters ul {
+            grid-template-columns: repeat(4, 1fr);
+        }
+    }
+</style>
+
+
+<body>
+
+
+    <h1>Nyhetsbrev</h1>
+    <?php if (!empty($newsletters)): ?>
+        <div class="newsletters">
+            <ul>
+                <?php foreach ($newsletters as $field => $newsletter): ?>
+                    <li class="newletter-item">
+                        <?= view('components/newsletter_card', ['name' => $newsletter['name'], 'date' => $newsletter['updated_at']]) ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+</body>
+
+</html>
