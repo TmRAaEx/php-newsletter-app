3e11b28ac3368c389c46a07e594aebffb0332350
TmRAaEx <tmr.aaex@gmail.com>
2025-05-19 14:14:35 +0200

    Added filter that automatically redirects to profile if you are logged in and try to go to login or reggister

--- newsletter-app/app/Config/Routes.php
+++ newsletter-app/app/Config/Routes.php
@@ -14,16 +14,21 @@ $routes->setAutoRoute(false);
 
 //public pages
 $routes->get('/', 'Pages::home');
-
-// Auth pages
-$routes->get('/register', 'Auth::register');
-$routes->post('/register', 'Auth::register');
-$routes->get('/login', 'Auth::login');
-$routes->post('/login', 'Auth::login');
-//-----------------------------
 $routes->get('/newsletters', 'Newsletters::index');
 $routes->get('/message', 'Pages::message');
 
+
+
+
+
+// Auth pages, only accesible if not logged in
+$routes->group('', ['filter' => 'noauth'], function ( $routes) {
+    $routes->get('/register', 'Auth::register');
+    $routes->get('/login', 'Auth::login');
+    //post requests
+    $routes->post('/register', 'Auth::register');
+    $routes->post('/login', 'Auth::login');
+});
 
 
 //pages that require an account
@@ -33,7 +38,7 @@ $routes->group('', ['filter' => 'auth'], function ($routes) {
     $routes->post('/logout-all', 'Auth::logoutAll');   
 });
 
-// pages that require an subscriber account
+// pages that require a subscriber account
 $routes->group('', ['filter' => 'subscriber'], function ($routes) {
     $routes->get('/newsletters/(:num)', 'Newsletters::single/$1');
     $routes->post('/newsletters/subscribe', 'Newsletters::subscribe');
