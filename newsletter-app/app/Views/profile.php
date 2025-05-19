e6e766ff84415169137ee219055cabe217d9c3a7
tmraaex <tmr.aaex@gmail.com>
2025-05-19 12:25:37 +0200

    Added sessions to the profile page

--- newsletter-app/app/Views/profile.php
+++ newsletter-app/app/Views/profile.php
@@ -15,8 +15,19 @@
         <p>Efternamn: <?= esc($user['last_name']) ?></p>
         <p>Email: <?= esc($user['email']) ?></p>
         <p>Registrerad: <?= esc($user['created_at']) ?></p>
-
     <?php endif ?>
+    <h2>Inloggade enheter</h2>
+    <?php if (!empty($sessions)): ?>
+        <ul>
+            <?php foreach ($sessions as $session): ?>
+                <li>
+                    <p>IP: <?= esc($session['ip_address']) ?></p>
+                    <p>Enhet: <?= esc($session['user_agent']) ?></p>
+                    <p>Senast Aktivitet: <?= esc($session['created_at']) ?></p>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 </body>
 
 </html>
