f633d9469a06ce04e51d4e885a5948464337ba89
TmRAaEx <tmr.aaex@gmail.com>
2025-05-19 13:43:32 +0200

    Made a semi static controller to display views that dont connect to DB

--- newsletter-app/app/Config/Routes.php
+++ newsletter-app/app/Config/Routes.php
@@ -8,7 +8,7 @@ use CodeIgniter\Router\RouteCollection;
 
 
 $routes->setAutoRoute(false);
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Pages::home');
 $routes->get('/register', 'Auth::register');
 $routes->post('/register', 'Auth::register');
 $routes->get('/login', 'Auth::login');
@@ -16,7 +16,9 @@ $routes->post('/login', 'Auth::login');
 $routes->get('/newsletters', 'Newsletters::index');
 
 $routes->group('', ['filter' => 'auth'], function ($routes) {
-    $routes->get('/profile', 'Profile::index');    
+    $routes->get('/profile', 'Profile::index'); 
+    $routes->post('/logout', 'Auth::logout');   
+    $routes->post('/logout-all', 'Auth::logoutAll');   
 });
 
 $routes->group('', ['filter' => 'subscriber'], function ($routes) {
